62d7e0b1f9e6d4b13a404d4de6ce63caa572fa55
Harshil Tomar <harshiltomar20@gmail.com>
2024-04-04 08:20:41 +0530

    1.1
    prisma initalised. migrations created for sql

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 - Object R. Model: Prisma, with connection pooling
 - Database: Postgres
 - Auth: Jwt + cookies
+
+# Backend
+
+Backend will be ideally having 4+1 routes
+
+- POST: /api/v1/signup
+- POST: /api/v1/signin
+- POST: /api/v1/blog
+- PUT: /api/v1/blog
+- GET: /api/v1/blog
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Bloging krenge vro!");
+});
 
-export default app
+app.post("/api/v1/signup", (c) => {
+  return c.text("Sign up Route");
+});
+
+app.post("/api/v1/signin", (c) => {
+  return c.text("Sign in Route");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("Blog Post Route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Blog Update Route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("Get Blog Route");
+});
+
+export default app;
