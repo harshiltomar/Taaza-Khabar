6bcdbd9872a3d607b0db36488533b90f25b1d462
Harshil Tomar <harshiltomar20@gmail.com>
2024-04-05 07:29:32 +0530

    1.1
    basic routes created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,64 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign, jwt } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
 
 app.get("/", (c) => {
-  return c.text("Bloging krenge vro!");
+  return c.text("Online hu vai");
 });
 
-app.post("/api/v1/signup", (c) => {
-  return c.text("Sign up Route");
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    //@ts-ignore
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  const token = await sign({ id: user.id }, "secret");
+  return c.json({
+    jwt: token,
+  });
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("Sign in Route");
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "User not found" });
+  }
+
+  //sign the jwt and return it to the user
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
 });
 
 app.post("/api/v1/blog", (c) => {
@@ -26,6 +73,10 @@ app.get("/api/v1/blog/:id", (c) => {
   const id = c.req.param("id");
   console.log(id);
   return c.text("Get Blog Route");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Bohot Sare Blogs milgye vro");
 });
 
 export default app;
